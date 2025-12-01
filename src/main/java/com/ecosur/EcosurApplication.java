74c1ecad7cc12cd5fee0c26b42e88097aff6858b
Maxime HERY <maximehery18@outlook.fr>
2025-12-01 11:16:12 +0100

    feat: initialize project

---
+++ src/main/java/com/ecosur/EcosurApplication.java
@@ -0,0 +1,12 @@
+package com.ecosur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcosurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcosurApplication.class, args);
+    }
+}
---
+++ src/test/java/com/ecosur/EcosurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecosur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcosurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
